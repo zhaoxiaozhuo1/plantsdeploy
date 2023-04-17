af350c4f110571d15f8b0727324f9b82e49b524a
zhaoxiaozhuo1 <100245734+zhaoxiaozhuo1@users.noreply.github.com>
2023-04-17 14:34:12 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,43 @@
 import streamlit as st
+import pymysql
 from firstpage import *
 from opencv import *
 st.set_page_config(page_title="YSU-农作物健康识别系统",layout="wide")
 
+con = pymysql.connect(host="10.51.105.79", user="root", password="zhaozhuo2001.", database="plants_disease", charset="utf8")
+
+c = con.cursor()
+
+def create_usertable():
+    c.execute('CREATE TABLE IF NOT EXISTS userstable(username TEXT, password TEXT)')
+
+def add_userdata(username, password):
+    if c.execute('SELECT username FROM userstable WHERE username = %s',(username)):
+        st.warning("用户名已存在，请更换一个新的用户名。")
+    else:
+        c.execute('INSERT INTO userstable(username,password) VALUES(%s,%s)',(username,password))
+        con.commit()
+        st.success("恭喜，您已成功注册。")
+        st.info("请在左侧选择“登录”选项进行登录。")
+
+def login_user(username,password):
+    if c.execute('SELECT username FROM userstable WHERE username = %s',(username)):
+        c.execute('SELECT * FROM userstable WHERE username = %s AND password = %s',(username,password))
+        data=c.fetchall()
+       # print("数据是",data)
+        return data
+    else:
+        st.warning("用户名不存在，请先选择注册按钮完成注册。")
+
+def view_all_users():
+    c.execute('SELECT * FROM userstable')
+    data = c.fetchall()
+    return data
+def show_slove(dname1):
+    c.execute('SELECT * FROM information WHERE dname= %s ',(dname1))
+    data=c.fetchall()
+    print("数据是",data)
+    return data
 
 def main():
     menu = ["首页","登录","注册", "注销"]
@@ -43,9 +78,28 @@ def main():
         username = st.sidebar.text_input("用户名")
         password = st.sidebar.text_input("密码",type = "password")
         if st.sidebar.checkbox("开始登录"):
-            dname1=str(main_loop())
-            print("名称是",dname1)
+            logged_user = login_user(username,password)
+            if logged_user:
+                st.session_state.count += 1
+
+                if st.session_state.count >= 1:
+                    st.sidebar.success("您已登录成功，您的用户名是 {}".format(username))
+                    #st.title("成功登录后可以看到的内容")
+                    st.balloons()
+                    dname1=str(main_loop())
+                    print("名称是",dname1)
                     #dname=main_loop()
+                    slove_data=show_slove(dname1)
+                    #print(type(slove_data))
+                    #for row in slove_data:
+                     #   print("ee12e")
+                        #print("%d--%d" % (cloumn[0], cloumn[1]))
+                    for i in slove_data:
+                        #print(i)
+                        dinfo=i[2]
+                        dsolve=i[3]
+                        st.success(dinfo)
+                        st.success(dsolve)
 
       #              c1, c2 = st.columns(2)
        #             with c1:
@@ -54,6 +108,9 @@ def main():
           #          with c2:
            #             st.success()
             #            st.image()
+                else:
+
+                    st.sidebar.warning("用户名或者密码不正确，请检查后重试。")
     elif choice =="注册":
         #st.subheader("注册")
         backimage = Image.open('sunrise.jpg')
@@ -63,7 +120,8 @@ def main():
         new_password1 = st.sidebar.text_input("确认密码",type = "password")
         if st.sidebar.button("注册"):
             if(new_password==new_password1):
-                st.info("注册成功。")
+                create_usertable()
+                add_userdata(new_user,new_password)
             else:
                 st.sidebar.warning("两次密码不正确，请检查后重试。")
     elif choice =="注销":
